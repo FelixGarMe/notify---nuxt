f8d2f1b27b29332b976ed04f2b7051bc85ecc6ef
FelixGarMe <mzpro123@gmail.com>
2024-06-11 18:21:17 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="showNotification">Mostrar Notificación</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    showNotification() {
+      this.$toast('¡Motor en peligro!');
+    }
+  }
+}
+</script>
+
+<style>
+/* Tu estilo aquí */
+</style>
